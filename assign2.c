b491a695ea29f0229cf6478c74db4b938a441a31
MyusufA <62134869+MyusufA@users.noreply.github.com>
2020-03-13 16:09:56 +0700

    Create assign2.c

---
+++ assign2.c
@@ -0,0 +1,21 @@
+/*File : asign2.c */
+
+ 
+#include <stdio.h>
+
+int main ()
+{
+/* KAMUS */
+int usia = 18;
+float berat = 68.30;
+double tinggi = 169.43;
+char jenis_kelamin = 'L';
+
+/* Algoritma */
+
+printf ("Usia: %i tahun\n, usia");
+printf ("Tinggi: %.2f cm\n, tinggi");
+printf ("Berat: %.2f kg\n, berat");
+printf ("Jenis Kelamin: %c\n, jenis_kelamin");
+return 0;
+}
